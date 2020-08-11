b6b6fe407e7923e8512d1fca0dc3926bf8f52585
SirWindfield <SirWindfield@users.noreply.github.com>
2020-08-11 18:00:33 +0000

    deploy: 199bbe485d9719f7ff0974544bf723f80eda7388

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"npm_package_json":{"doc":"","i":[],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["npm_package_json"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
