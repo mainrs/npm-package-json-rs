92aa347def755010f68d353066787c94def5bc23
SirWindfield <SirWindfield@users.noreply.github.com>
2020-08-12 23:38:09 +0000

    deploy: b9db1b76919a0e54737a4f314f3d2f6331b57b4e

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["npm_package_json"] = [{"text":"impl Display for Error","synthetic":false,"types":[]}];
+implementors["npm_package_json"] = [{"text":"impl Display for Error","synthetic":false,"types":[]},{"text":"impl Display for Person","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
